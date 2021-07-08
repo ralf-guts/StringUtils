fec6b10cceeb22e3e2f4edf0e58c02e9dfa64a76
wangpeng <wangpenghs@hangsheng.com.cn>
2021-07-08 12:20:29 +0800

    initial

---
+++ cmake-build-debug/CMakeFiles/StringUtils.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/StringUtils.dir/main.c.o"
+  "StringUtils"
+  "StringUtils.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/StringUtils.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
